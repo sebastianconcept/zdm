f1fc49e1b37f95b511ec0cfd8f35f8c722f1c685
Sebastian Sastre <sebastian@flowingconcept.com>
2014-06-22 16:46:06 -0300

    reacting to edit button

--- client/question/behaviour.js
+++ client/question/behaviour.js
@@ -1,23 +1,41 @@
  Template.question.created = function() {
     // which questions are in edit mode? default: none
-    Template.question.editors=[];
+    Session.set('questionEditors',[]);
+
+    //Template.question.editing = false;
+
+
+    // using ReactiveDic for editor states as described:
+    // https://atmospherejs.com/package/reactive-dict-bind
+    //Template.question.editors = new ReactiveDict;
+    //dictBind(Template.question);
   };
 
   // answers true if the question with aQuestionId
   // is in edit mode
   Template.question.isEditing = function(){
-    console.log(Session.get('questionEditors'));
+    //console.log(Session.get('questionEditors'));
     //return Template.question.editors.indexOf(this._id)!= -1;
-    return Session.get('questionEditors').indexOf(this._id)!= -1;
+    //return Session.get('questionEditors').indexOf(this._id)!= -1;
+    //return Template.question.editing;
+    //return Session.get('isEditing');
+    return Session.get('questionEditors').indexOf(this._id)!= -1
   }; 
 
   // Adds the question's id to the ones in edit mode
   Template.question.edit = function(aQuestion){
     if(aQuestion != null &&
-      (Template.question.editors.indexOf(aQuestion._id)== -1)) {
-      this.editors.push(aQuestion._id)};
-    console.log(this);
-    this.render();
+      (Session.get('questionEditors').indexOf(aQuestion._id)== -1)) {
+        //Template.question.editing = true;
+        //Session.set('isEditing',true);
+        //Session.get('questionEditors').push(aQuestion._id)
+        var editing = Session.get('questionEditors');
+        editing.push(aQuestion._id);
+        Session.set('questionEditors',editing);
+      };
+    //console.log(Session.get('questionEditors'));
+    //console.log(this);
+    //Template.question.reset();
   };
 
   Template.question.removeQuestion = function(aQuestion){
