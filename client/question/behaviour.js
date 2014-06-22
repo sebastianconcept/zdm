63f7c7ecd6a2d1cdd69ac2f25d0a0c39ba78fdeb
Sebastian Sastre <sebastian@flowingconcept.com>
2014-06-22 16:59:29 -0300

    done button closes editing mode

--- client/question/behaviour.js
+++ client/question/behaviour.js
@@ -2,9 +2,6 @@
     // which questions are in edit mode? default: none
     Session.set('questionEditors',[]);
 
-    //Template.question.editing = false;
-
-
     // using ReactiveDic for editor states as described:
     // https://atmospherejs.com/package/reactive-dict-bind
     //Template.question.editors = new ReactiveDict;
@@ -14,11 +11,6 @@
   // answers true if the question with aQuestionId
   // is in edit mode
   Template.question.isEditing = function(){
-    //console.log(Session.get('questionEditors'));
-    //return Template.question.editors.indexOf(this._id)!= -1;
-    //return Session.get('questionEditors').indexOf(this._id)!= -1;
-    //return Template.question.editing;
-    //return Session.get('isEditing');
     return Session.get('questionEditors').indexOf(this._id)!= -1
   }; 
 
@@ -26,30 +18,31 @@
   Template.question.edit = function(aQuestion){
     if(aQuestion != null &&
       (Session.get('questionEditors').indexOf(aQuestion._id)== -1)) {
-        //Template.question.editing = true;
-        //Session.set('isEditing',true);
-        //Session.get('questionEditors').push(aQuestion._id)
         var editing = Session.get('questionEditors');
         editing.push(aQuestion._id);
         Session.set('questionEditors',editing);
       };
-    //console.log(Session.get('questionEditors'));
-    //console.log(this);
-    //Template.question.reset();
   };
+
+  Template.question.saveQuestion = function(aQuestion){
+    //console.log(aQuestion);
+    Questions.update(aQuestion._id,aQuestion);
+    var editing = Session.get('questionEditors');
+    editing.splice(editing.indexOf(aQuestion._id),1);
+    Session.set('questionEditors',editing);
+  }; 
 
   Template.question.removeQuestion = function(aQuestion){
     Questions.remove(aQuestion._id);
-}; 
+  }; 
 
 
 Template.question.events({
-  'click li': function(){
-    //Template.question.selectedQuestion=this._id;
-    //console.log(Template.question.selectedQuestion);
-  },
   'click .editQuestion': function(){
     Template.question.edit(this);
+  },
+  'click .saveQuestion': function(){
+    Template.question.saveQuestion(this);
   },
   'click .removeQuestion': function(){
     Template.question.removeQuestion(this);
